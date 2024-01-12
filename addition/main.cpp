e828dba3eeb5eec1462e7fe7d1571b83cc93dd89
ghousebiai01 <ghousebiai01@gmail.com>
2024-01-12 13:48:29 +0530

    added

---
+++ addition/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+
+   cout<<"\n\n\t\t***Arithmatic Examples in C ++lang***\\n\n";
+   int value1,value2,total,op,sub;
+    cout <<"\nEnter the value a\n";
+    cin >> value1;
+    cout <<"\nEnter the value b\n";
+    cin >> value2;
+    total=value1+value2;
+    cout<<"The Added value of A and B is= "<<total;
+     return 0;
+
+
+}
